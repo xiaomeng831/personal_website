fc9086f5b1cc100576bd0ffa0e6bfddc99978e09
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2022-12-18 15:14:03 -0500

    commit

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -45,11 +45,7 @@ function ResumeNew() {
             <Row className="resume">
               <Document file={resumeLink} className="d-flex justify-content-center">
                 <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-              </Document>
-              <div style={{height:"20px"}}></div>
-              <Document file={resumeLink} className="d-flex justify-content-center">
-                <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-              </Document>
+              </Document>              
             </Row>
 
             
